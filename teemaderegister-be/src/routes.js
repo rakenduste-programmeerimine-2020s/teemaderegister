7b8b14cf3badc93c905d8de501ab7c4a9af1d05c
Marten Jürgens <55079581+MJ1400@users.noreply.github.com>
2020-12-17 22:06:06 +0200

    Fixed linting issues and changed API url

--- teemaderegister-be/src/routes.js
+++ teemaderegister-be/src/routes.js
@@ -16,98 +16,35 @@ const topics = require('./controllers/topics')
 const users = require('./controllers/users')
 const admin = require('./controllers/admin')
 
-router.post(
-  '/auth/local/login',
-  validate.localLogin,
-  asyncMiddleware(auth.localLogin)
-)
-router.post(
-  '/auth/local/signup',
-  validate.localSignup,
-  asyncMiddleware(auth.localSignup)
-)
+router.post('/auth/local/login', validate.localLogin, asyncMiddleware(auth.localLogin))
+router.post('/auth/local/signup', validate.localSignup, asyncMiddleware(auth.localSignup))
 router.post('/auth/logout', jwtEnsure, asyncMiddleware(auth.logout))
-router.post(
-  '/auth/forgot',
-  validate.passwordResetEmail,
-  asyncMiddleware(auth.forgotPassword)
-)
-router.get(
-  '/auth/reset/:token',
-  asyncMiddleware(auth.validatePasswordResetToken)
-)
-router.post(
-  '/auth/reset/:token',
-  validate.passwordResetMatch,
-  asyncMiddleware(auth.updatePassword)
-)
+router.post('/auth/forgot', validate.passwordResetEmail, asyncMiddleware(auth.forgotPassword))
+router.get('/auth/reset/:token', asyncMiddleware(auth.validatePasswordResetToken))
+router.post('/auth/reset/:token', validate.passwordResetMatch, asyncMiddleware(auth.updatePassword))
 
 router.get('/curriculums/', asyncMiddleware(curriculums.getCurriculums))
-router.get(
-  '/curriculums/:slug',
-  asyncMiddleware(curriculums.getCurriculumBySlug)
-)
-router.post(
-  '/curriculums',
-  jwtEnsure,
-  allowRoles([ADMIN]),
-  validate.addCurriculumValidation,
-  asyncMiddleware(curriculums.postCurriculums)
-)
+router.get('/curriculums/:slug', asyncMiddleware(curriculums.getCurriculumBySlug))
+router.post('/curriculums', jwtEnsure, allowRoles([ADMIN]), validate.addCurriculumValidation, asyncMiddleware(curriculums.postCurriculums))
 
 router.get('/search/counts', asyncMiddleware(search.getCounts))
 
 router.get('/supervisors/', asyncMiddleware(supervisors.getSupervisors))
-router.get(
-  '/supervisors/curriculumForm/',
-  asyncMiddleware(supervisors.getSupervisorsCurriculumForm)
-)
-router.get(
-  '/supervisors/:slug',
-  asyncMiddleware(supervisors.getSupervisorBySlug)
-)
+router.get('/supervisors/curriculumForm/', asyncMiddleware(supervisors.getSupervisorsCurriculumForm))
+router.get('/supervisors/:slug', asyncMiddleware(supervisors.getSupervisorBySlug))
 
 router.get('/topics/', asyncMiddleware(topics.getTopics))
 
 router.get('/users/me', jwtEnsure, asyncMiddleware(users.getUser))
 router.get('/users/profile', jwtEnsure, asyncMiddleware(users.getProfile))
-router.put(
-  '/users/profile',
-  jwtEnsure,
-  validate.userAccountUpdate,
-  asyncMiddleware(users.updateUser)
-)
-router.put(
-  '/users/password',
-  jwtEnsure,
-  validate.userPasswordUpdate,
-  asyncMiddleware(users.updatePassword)
-)
-router.post(
-  '/users/upload-picture',
-  jwtEnsure,
-  multerMiddleware('profileImage'),
-  asyncMiddleware(users.uploadPicture)
-)
-router.put(
-  '/users/reset-picture',
-  jwtEnsure,
-  asyncMiddleware(users.resetPicture)
-)
+router.put('/users/profile', jwtEnsure, validate.userAccountUpdate, asyncMiddleware(users.updateUser))
+router.put('/users/password', jwtEnsure, validate.userPasswordUpdate, asyncMiddleware(users.updatePassword))
+router.post('/users/upload-picture', jwtEnsure, multerMiddleware('profileImage'), asyncMiddleware(users.uploadPicture))
+router.put('/users/reset-picture', jwtEnsure, asyncMiddleware(users.resetPicture))
 
 // SAMPLE
-router.get(
-  '/admin/',
-  jwtEnsure,
-  allowRoles([ADMIN]),
-  asyncMiddleware(admin.getSecret)
-)
-
-router.get(
-  '/getAllUsers',
-  jwtEnsure,
-  allowRoles([ADMIN]),
-  asyncMiddleware(users.getAllUsers)
-)
+router.get('/admin/', jwtEnsure, allowRoles([ADMIN]), asyncMiddleware(admin.getSecret))
+router.post('/admin/createUser', jwtEnsure, allowRoles([ADMIN]), asyncMiddleware(admin.createUser))
+router.get('/admin/users', jwtEnsure, allowRoles([ADMIN]), asyncMiddleware(users.getAllUsers))
 
 module.exports = router
