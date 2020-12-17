e792a9fae512b97c90deb838645b6b1255aed2d5
Gertin Pakkonen <gertinnn@tlu.ee>
2020-12-17 20:11:57 +0200

    Admin can create user (issue #7) (#32)
    * fix: search query param #7
    
    * feat: added form #7
    
    * Admin can create user (#28)
    
    * Admin can almost create user!
    
    * Minor changes
    
    * Added email sending and POST API
    
    Co-authored-by: 0x0n0x <gghalfer@gmail.com>
    
    * Admin can create user (#29)
    
    * Admin can almost create user!
    
    * Minor changes
    
    * Added email sending and POST API
    
    * Created mail template
    
    * Added password create text
    
    * Working thing - some auth errors tho when creating 2 accounts
    
    * Working thing!
    
    * Working thing!
    
    * Changed dispatching events
    
    * Working thing!
    
    * Working thing!
    
    Co-authored-by: 0x0n0x <gghalfer@gmail.com>
    Co-authored-by: 0x0n0x <62573802+0x0n0x@users.noreply.github.com>
    
    * Admin can create user (#31)
    
    * Admin can almost create user!
    
    * Minor changes
    
    * Added email sending and POST API
    
    * Created mail template
    
    * Added password create text
    
    * Working thing - some auth errors tho when creating 2 accounts
    
    * Working thing!
    
    * Working thing!
    
    * Changed dispatching events
    
    * Working thing!
    
    * Working thing!
    
    * Admin can create user (#29) (#30)
    
    * Admin can almost create user!
    
    * Minor changes
    
    * Added email sending and POST API
    
    * Created mail template
    
    * Added password create text
    
    * Working thing - some auth errors tho when creating 2 accounts
    
    * Working thing!
    
    * Working thing!
    
    * Changed dispatching events
    
    * Working thing!
    
    * Working thing!
    
    Co-authored-by: 0x0n0x <gghalfer@gmail.com>
    Co-authored-by: 0x0n0x <62573802+0x0n0x@users.noreply.github.com>
    
    Co-authored-by: 0x0n0x <gghalfer@gmail.com>
    Co-authored-by: 0x0n0x <62573802+0x0n0x@users.noreply.github.com>
    
    Co-authored-by: 0x0n0x <gghalfer@gmail.com>
    Co-authored-by: 0x0n0x <62573802+0x0n0x@users.noreply.github.com>
    
    * Fixed pull request problems
    
    * Fixed backend problem with unnecessary if statement, removed redundant action type and also used useEffect for calling setDocTitle
    
    * useEffect for calling setDocTitle
    
    Co-authored-by: 0x0n0x <gghalfer@gmail.com>
    Co-authored-by: 0x0n0x <62573802+0x0n0x@users.noreply.github.com>

--- teemaderegister-be/src/controllers/admin.js
+++ teemaderegister-be/src/controllers/admin.js
@@ -1,5 +1,46 @@
-module.exports.getSecret = async (req, res) => {
-  const { user: { _id } } = req
+const User = require('../models/user')
+const slug = require('slug')
+const mail = require('./../utils/mail')
+const {getPasswordResetTokenValues} = require('./auth')
 
-  return res.json({ _id })
+module.exports.createUser = async (req, res) => {
+  const {firstName, lastName, email, role} = req.body
+  const slugs = `${slug(firstName)}-${slug(lastName)}`
+
+  const existingUser = await User.findOne({'login.email': email})
+  if (existingUser) {
+    return res.status(406).json({message: 'Email already in use!', success: 0})
+  }
+  const user = {
+    profile: {
+      firstName: firstName,
+      lastName: lastName,
+      slug: slugs
+    },
+
+    login: {
+      email: email,
+      roles: role,
+      ...getPasswordResetTokenValues()
+    }
+  }
+  const passwordCreateURL = `${process.env.SITE_URL}/account/password/${user.login.passwordResetToken}?is-new=1`
+
+  await new User(user).save()
+
+  await mail.sendMail({
+    to: email,
+    subject: `Password create`,
+    template: {
+      name: 'createPassword',
+      data: {passwordCreateURL}
+    }
+  })
+
+  return res.status(201).json({message: 'Created successfully!', success: 1})
+}
+module.exports.getSecret = async (req, res) => {
+  const {user: {_id}} = req
+
+  return res.json({_id})
 }
