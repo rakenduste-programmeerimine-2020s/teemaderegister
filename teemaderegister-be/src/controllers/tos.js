a1408c68feda7d070c45bd39da90af563413c2f6
stenmr <stenmr@tlu.ee>
2021-01-03 20:33:27 +0200

    Use array destructuring shorthand

--- teemaderegister-be/src/controllers/tos.js
+++ teemaderegister-be/src/controllers/tos.js
@@ -1,23 +1,21 @@
 const TermsOfService = require('../models/tos')
 
 module.exports.getTos = async (req, res) => {
-  const tos = await TermsOfService.find({})
+  const [tos] = await TermsOfService.find({})
 
-  const doc = tos[0]
-
-  return res.json({ doc })
+  return res.json({ tos })
 }
 
 module.exports.saveTos = async (req, res) => {
   const { content } = req.body
 
-  const tos = await TermsOfService.find({})
+  const [tos] = await TermsOfService.find({})
 
-  if (tos.length === 0) {
+  if (tos) {
     await new TermsOfService({ content }).save()
   } else {
-    tos[0].content = content
-    await tos[0].save()
+    tos.content = content
+    await tos.save()
   }
 
   return res.json({ message: 'Terms of Service were successfully changed' })
